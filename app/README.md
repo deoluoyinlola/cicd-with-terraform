6bbc033d4f633e641e2497ee5b1b8683d64ecc42
Adeolu Oyinlola <adeolu.oyinlola@alerzo.com>
2022-12-07 15:22:26 +0100

    refactor

---
+++ app/README.md
@@ -0,0 +1,2 @@
+# internpert
+An open community of young Africa talents seeking network and hands-on experience.
---
+++ app/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Coming Soon v6.0.6 (https://internpert.com)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/internpert-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
