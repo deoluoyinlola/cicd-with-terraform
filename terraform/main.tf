2322ab5d8a52a64a7a0c879ae00881a0640de949
Adeolu Oyinlola <adeolu.oyinlola@alerzo.com>
2022-12-07 16:01:57 +0100

    tf init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 terraform {
+   backend "s3" {
+     bucket         = "first-tf-state"
+     key            = "global/s3/terraform.tfstate"
+     region         = "us-east-1"
+     access_key     = "AKIA52WMJFUL7YM5CFIY"
+     secret_key     = "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"
+     dynamodb_table = "terraform-state-locking"
+     encrypt        = true
+   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,8 +17,38 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-east-1"
   access_key = "AKIA52WMJFUL7YM5CFIY"
   secret_key = "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"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "first-tf-state"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
