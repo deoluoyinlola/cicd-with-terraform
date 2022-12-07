6d33fd4f30485283d4f02857dee7a05e69eabc63
Adeolu Oyinlola <adeolu.oyinlola@alerzo.com>
2022-12-07 19:47:06 +0100

    route-table add

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,32 @@ resource "aws_subnet" "myapp-subnet-1" {
       Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_internet_gateway" "myapp-igw" {
+	vpc_id = aws_vpc.myapp-vpc.id
+    
+    tags = {
+     Name = "${var.env_prefix}-internet-gateway"
+   }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+   vpc_id = aws_vpc.myapp-vpc.id
+
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+
+   # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
+
+   tags = {
+     Name = "${var.env_prefix}-route-table"
+   }
+}
+
+# Associate subnet with Route Table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
